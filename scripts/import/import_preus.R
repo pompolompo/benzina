e8e1bd65009565c66c1e159ed5f5dc49add262b3
pompolompo <enferrofa@gmail.com>
2025-03-26 23:08:39 +0100

    furrr does not help :/

--- scripts/import/import_preus.R
+++ scripts/import/import_preus.R
@@ -1,14 +1,13 @@
 # Libraries ---------------------------------------------------------------
 library(dplyr)
 library(purrr)
-library(furrr)
 library(httr2)
 library(DBI)
 
 # Options -----------------------------------------------------------------
 ccaa <- "09" # catalunya
 prod <- "01" # benzina
-prev <- as.Date("2015-03-01")
+prev <- as.Date("2023-06-23")
 dies <- seq.Date(from = prev, to = Sys.Date() - 2, by = "day") %>%
   format("%d-%m-%Y")
 
@@ -16,13 +15,12 @@ dies <- seq.Date(from = prev, to = Sys.Date() - 2, by = "day") %>%
 # Database ----------------------------------------------------------------
 con <- dbConnect(RSQLite::SQLite(), "dbase/sqlite/benzina.sqlite")
 
-
 # Download and Save -------------------------------------------------------
 temps <- Sys.time()
-plan(workers = 3)
-split(dies, cut(seq_along(dies), breaks = round(length(dies) / 30))) %>%
-  future_walk(\(interval){
-    interval %>%
+
+split(dies, cut(seq_along(dies), breaks = round(length(dies) / 100))) %>%
+  walk(\(interval){
+    preus <- interval %>%
       map(\(dia){
         paste0(
           "https://sedeaplicaciones.minetur.gob.es",
@@ -37,7 +35,7 @@ split(dies, cut(seq_along(dies), breaks = round(length(dies) / 30))) %>%
           bind_rows() %>%
           janitor::clean_names() %>%
           mutate(across(
-            c(latitud, longitud_wgs84, precio_producto),
+            matches("latitud|longitud_wgs84|precio_producto"),
             \(x) as.numeric(gsub(",", ".", x))
           )) %>%
           mutate(dia = dia)
@@ -45,5 +43,6 @@ split(dies, cut(seq_along(dies), breaks = round(length(dies) / 30))) %>%
       bind_rows() %>%
       dbAppendTable(con, "preus", .)
   })
+
 temps <- Sys.time() - temps
 dbDisconnect(con)
